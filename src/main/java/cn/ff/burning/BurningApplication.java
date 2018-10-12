47cc82f968e88e98754d40191fcb472773373270
fengfan <dotff@qq.com>
2018-10-12 15:15:47 +0800

    init

---
+++ src/main/java/cn/ff/burning/BurningApplication.java
@@ -0,0 +1,12 @@
+package cn.ff.burning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurningApplication.class, args);
+    }
+}
